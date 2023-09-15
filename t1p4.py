ab17a60c628b3c02ef64a6742b2c860ead17f4c0
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-09-15 16:51:46 -0400

    Added Sleep

--- t1p4.py
+++ t1p4.py
@@ -4,6 +4,8 @@
 # Created Sept 13th 2023
 # This program determines if a car can go through an intersection.
 
+from time import sleep
+
 def intersection(light_color, time_to_intersection):
     """Decides if a car can go through an intersection and prints"""
     if light_color == "green":
@@ -18,19 +20,19 @@ def intersection(light_color, time_to_intersection):
 def main():
     """Main function that starts the program"""
     # intro and input
-    print("\nShould you go through an intersection?\n")
+    print("Should you go through an intersection?\n")
     light_color = input("What color is the light? ")
-    distance = input("How far (in m) are you from the intersection? ")
-    speed = input("How fast (in m/s) are you going? ")
+    distance = float(input("How far (in m) are you from the intersection? "))
+    speed = float(input("How fast (in m/s) are you going? "))
 
-    # data validation and intersection() function
+    # Data validation and intersection() function
     try:
-        time_to_intersection = float(distance) / float(speed)
+        time_to_intersection = distance / speed
         intersection(light_color, time_to_intersection)
     except Exception:
         print("\nError, make sure distance and speed are numbers.")
 
-    print("\nDone.")
+    sleep(1) # So the file does not instantly close when it is finished running
 
 if __name__ == "__main__":
     main()
